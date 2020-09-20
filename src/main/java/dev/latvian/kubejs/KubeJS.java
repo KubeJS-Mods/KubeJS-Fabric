9b6e8e2a8fbf2ab55bb0ae8e6bb02377c38e20ad
shedaniel <daniel@shedaniel.me>
2020-09-20 22:59:38 +0800

    why does java use stupid versioning

--- src/main/java/dev/latvian/kubejs/KubeJS.java
+++ src/main/java/dev/latvian/kubejs/KubeJS.java
@@ -44,8 +44,21 @@ public class KubeJS implements ModInitializer {
 	
 	public static ScriptManager startupScriptManager, clientScriptManager;
 	
+	private static int getJavaVersion() {
+		String version = System.getProperty("java.version");
+		if (version.startsWith("1.")) {
+			version = version.substring(2, 3);
+		} else {
+			int dot = version.indexOf(".");
+			if (dot != -1) {
+				version = version.substring(0, dot);
+			}
+		}
+		return Integer.parseInt(version);
+	}
+	
 	public KubeJS() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-		if (Double.parseDouble(System.getProperty("java.version")) >= 15) {
+		if (getJavaVersion() >= 15) {
 			System.err.println("\n\n================================================================================\nKubeJS currently does not support Java 15 or above. Please use Java 14 or below while we work on this issue, sorry!\n================================================================================\n");
 			System.exit(0);
 		}
